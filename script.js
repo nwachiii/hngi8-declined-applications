1a192a13f34ce90389cbe6922d0afc3f92e1ba0c
Nwachi Emmanuel <egreatness1@gmail.com>
2020-07-28 10:11:11 +0100

    fixes: alignments

--- script.js
+++ script.js
@@ -16,7 +16,23 @@
 
 
 
+           //pagination
+// Get the container element
+var page = document.getElementById("pagination");
 
+// Get all buttons with class="btn" inside the container
+var btns = page.getElementsByClassName("btn");
+
+// Loop through the buttons and add the active class to the current/clicked button
+for (var i = 0; i < btns.length; i++) {
+  btns[i].addEventListener("click", function() {
+    var current = document.getElementsByClassName("active");
+    current[0].className = current[0].className.replace(" active", "");
+    this.className += " active";
+  });
+}
+
+var dir = page.getElementsByClassName("dir");
 
 
 
