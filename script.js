71d4c4cec68656d45f3f754a0c055130519f220a
Nwachi Emmanuel <egreatness1@gmail.com>
2020-07-28 09:26:03 +0100

    feat: pagination

---
+++ script.js
@@ -0,0 +1,33 @@
+
+
+
+/* Set the width of the sidebar to 250px and the left margin of the page content to 250px */
+// function openNav() {
+//   document.getElementById("mySidebar").style.width = "250px";
+//   document.getElementById("mainSidebar").style.marginLeft = "250px";
+// }
+
+/* Set the width of the sidebar to 0 and the left margin of the page content to 0 */
+// function closeNav() {
+//   document.getElementById("mySidebar").style.width = "0";
+//   document.getElementById("mainSidebar").style.marginLeft = "0";
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//an if statement to check for an active nav class and add the nav-active bar
